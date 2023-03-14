88bf882598da347205ceead4a35d7d01d21ebe61
Elias Warres <ewarres@Eliass-MacBook-Pro.local>
2023-03-13 20:51:19 -0700

    Get shards and replicas for all kvs functions

--- src/controllers/kvs.controller.ts
+++ src/controllers/kvs.controller.ts
@@ -49,7 +49,7 @@ class KvsController {
       }
       // broadcast write
       if (IORunning()) {
-        const broadcastData = { key: kvData.key, val: kvData.val, "causal-metadata": metadata, sender: ADDRESS, "shard-id": 1 };
+        const broadcastData = { key: kvData.key, val: kvData.val, "causal-metadata": metadata, sender: ADDRESS, "shard-id": shard_id };
         broadcast("kvs:write", broadcastData);
       }
     } catch (error) {
@@ -66,15 +66,20 @@ class KvsController {
       const receivedClock = clockService.parseReceivedClock(req.body[CAUSAL_METADATA_KEY]);
       const localClock = clockService.getVectorClock();
 
+      // getting replicas which contain all shards altogether
+      for (let shard_id = 0; shard_id < viewService.num_shards; shard_id++) {
+        let replicas: Promise<string[]> = viewService.getShardReplicas(shard_id);
+      }
+
       if (localClock.validateClock(receivedClock)) {
         const metadata = Array.from(localClock.getClock());
         const keys = await this.kvsService.getAllKeys();
-        res.status(200).json({ "causal-metadata": metadata, ...keys, "shard-id": 1 });
+        res.status(200).json({ "causal-metadata": metadata, ...keys });
       } else {
         const { success, value }: { success: boolean; value: any } = await clockService.getCausalConsistency(undefined, receivedClock);
         if (success) {
           const metadata = Array.from(localClock.getClock());
-          res.status(200).json({ "causal-metadata": metadata, ...value, "shard-id": 1 });
+          res.status(200).json({ "causal-metadata": metadata, ...value });
         } else {
           throw new HttpException(500, "timed out while waiting for depended updates");
         }
@@ -97,6 +102,9 @@ class KvsController {
       const receivedClock = clockService.parseReceivedClock(req.body[CAUSAL_METADATA_KEY]);
       const localClock = clockService.getVectorClock();
       const key = req.params.key as string;
+      // Get replicas
+      let shard_id = this.kvsService.lookUp(viewService.num_shards, key);
+      let replicas: Promise<string[]> = viewService.getShardReplicas(shard_id);
       if (localClock.validateClock(receivedClock)) {
         let val = await this.kvsService.getKv(key);
         if (IORunning() && val === undefined) {
@@ -115,9 +123,9 @@ class KvsController {
         }
         const metadata = Array.from(localClock.getClock());
         if (val === undefined) {
-          res.status(404).json({ "causal-metadata": metadata, "shard-id": 1 });
+          res.status(404).json({ "causal-metadata": metadata, "shard-id": shard_id });
         } else {
-          res.status(200).json({ val, "causal-metadata": metadata, "shard-id": 1 });
+          res.status(200).json({ val, "causal-metadata": metadata, "shard-id": shard_id });
         }
       } else {
         if (IORunning()) {
@@ -128,9 +136,9 @@ class KvsController {
           if (success) {
             const metadata = Array.from(localClock.getClock());
             if (exists) {
-              res.status(200).json({ val: value, "causal-metadata": metadata, "shard-id": 1 });
+              res.status(200).json({ val: value, "causal-metadata": metadata, "shard-id": shard_id });
             } else {
-              res.status(404).json({ "causal-metadata": metadata, "shard-id": 1 });
+              res.status(404).json({ "causal-metadata": metadata, "shard-id": shard_id });
             }
           } else {
             throw new HttpException(500, "timed out while waiting for depended updates");
@@ -158,15 +166,18 @@ class KvsController {
         const key = req.params.key as string;
         const prev = await this.kvsService.deleteKv(key);
         let metadata = Array.from(localClock.getClock());
+        // Get replicas
+        let shard_id = this.kvsService.lookUp(viewService.num_shards, key);
+        let replicas: Promise<string[]> = viewService.getShardReplicas(shard_id);
         if (prev === undefined) {
-          res.status(404).json({ "causal-metadata": metadata, "shard-id": 1 });
+          res.status(404).json({ "causal-metadata": metadata, "shard-id": shard_id });
         } else {
           localClock.incrementClock(ADDRESS);
           metadata = Array.from(localClock.getClock());
-          res.status(200).json({ "causal-metadata": metadata, "shard-id": 1 });
+          res.status(200).json({ "causal-metadata": metadata, "shard-id": shard_id });
           // broadcast delete
           if (IORunning()) {
-            const broadcastData = { key, "causal-metadata": metadata, sender: ADDRESS, "shard-id": 1 };
+            const broadcastData = { key, "causal-metadata": metadata, sender: ADDRESS, "shard-id": shard_id };
             broadcast("kvs:delete", broadcastData);
           }
         }
